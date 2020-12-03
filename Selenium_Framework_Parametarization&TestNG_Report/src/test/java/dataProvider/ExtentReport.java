7fc196f69b7d22b5b7eb1f8358669dc8217cf512
rakesh-tidke <rakeshtidke72@gmail.com>
2020-12-03 14:45:46 +0530

    Assignment completed

---
+++ Selenium_Framework_Parametarization&TestNG_Report/src/test/java/dataProvider/ExtentReport.java
@@ -0,0 +1,67 @@
+package dataProvider;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+public class ExtentReport {
+
+	WebDriver driver;
+
+	String url = "https://www.google.com/";
+	
+	ExtentHtmlReporter reporter;
+	ExtentReports extent;
+	ExtentTest logger;
+
+	@Before
+	public void windowSetup() throws Exception{
+
+		reporter = new ExtentHtmlReporter("./extentReport.html");
+		
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		
+		logger = extent.createTest("Google Search Test");
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\RAKESH\\Infosys\\ChromeDriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+	}
+
+	@After
+	public void teardown() {
+		driver.quit();
+		logger.pass("Closed the browser");
+		
+		logger.info("Test Completed");
+		
+		extent.flush();
+	}
+	
+	@Test
+	public void test() throws InterruptedException {
+		
+		logger.log(Status.INFO, "Starting Test");
+		
+		driver.get(url);
+		logger.pass("Navigated to Google.com");
+		
+		driver.findElement(By.name("q")).sendKeys("FC Bayern");
+		logger.pass("Added text in search box");
+		
+		driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[3]/center/input[1]")).submit();
+		logger.pass("Clicked Google Search button");
+		
+	}
+
+}
---
+++ Selenium_Framework_Parametarization&TestNG_Report/src/test/java/dataProvider/Testng.java
@@ -0,0 +1,43 @@
+package dataProvider;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
+
+import java.io.IOException;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+
+public class Testng {
+	@Test(dataProvider = "data")
+	public void f(Integer JerseyNo, String FirstName,String LastName) throws IOException {
+		System.out.println("******** FC Bayern Team ********");
+		System.out.println("Player No "+JerseyNo);
+		System.out.println(FirstName);
+		System.out.println(LastName);
+	}
+
+	@DataProvider(name="data")
+	public Object[][] information() {
+		Object[][] data = new Object[2][3];
+
+		data[0][0]=9;
+		data[0][1]="Robert";
+		data[0][2]="Lewandowski";
+
+		data[1][0]=25;
+		data[1][1]="Thomas";
+		data[1][2]="Muller";
+
+		return data;
+	}
+	@BeforeTest
+	public void beforeTest() {
+	}
+
+	@AfterTest
+	public void afterTest() {
+	}
+
+}
